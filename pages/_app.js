42ec5b3c768a0e906069de87524b967c5e493fd5
TECHIE SAM <blessingabolade15@gmail.com>
2023-05-16 17:01:55 +0100

    Added layout.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
+import Layout from '@/components/Layout'
 import SideBar from '@/components/SideBar'
 import 'styles/globals.css'
-export default function App({ Component, pageProps }) {
-  console.log(Component)
+export default function App({ Component, pageProps, }) {
   return (
     <>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   )
 }
