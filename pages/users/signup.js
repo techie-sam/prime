a1d447f61c6ac963c82511d92325233957448582
TECHIE SAM <blessingabolade15@gmail.com>
2023-05-30 15:40:52 +0100

    used formik for signup validation

--- pages/users/signup.js
+++ pages/users/signup.js
@@ -1,21 +1,13 @@
-import { useFormik } from "formik"
+import { ErrorMessage, Field, Form, Formik, useFormik } from "formik"
 import Link from "next/link"
+import * as Yup from 'yup';
 
 const SignUp = ({ formDetails }) => {
 
-  const initialValues={
-    firstName,
-    lastName,
-    email,
-    password,
-  }
-  const formik = useFormik({
 
-  })
+
   return (
     <>
-
-
       <section className="bg-gray-200 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -26,25 +18,52 @@ const SignUp = ({ formDetails }) => {
               </h1>
 
               {/*=============== Signup Form ==================*/}
-              <form className="space-y-4 md:space-y-6" action="#">
-                {
-                  formDetails.map(({ name, type, placeholder }) =>
-                    <div key={name} className="relative mb-6" data-te-input-wrapper-init>
-                      <label htmlFor={name} className="sr-only">Email address</label>
-                      <input
-                        key={name}
-                        type={type}
-                        name={name}
-                        className="block min-h-[auto] w-full rounded bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear dark:text-white ring-gray-400 ring-1 focus:ring-2 focus:ring-[#A10035] focus:border-transparent"
-                        id={name}
-                        placeholder={placeholder} />
-                    </div>
-                  )
-                }
-                <button type="submit" name="submit" className='bg-[#A10035] hover:bg-[#fb1361] font-semibold w-full rounded p-3 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#A10035]' >
-                  SIGN UP
-                </button>
-              </form>
+              <Formik
+                initialValues={{ firstName: '', lastName: '', email: '', password: '' }}
+                validationSchema={Yup.object({
+                  firstName: Yup.string()
+                    .max(20, 'First Name must be 20 characters or less')
+                    .required('First Name is required'),
+                  lastName: Yup.string()
+                    .max(20, 'Last Name be 20 characters or less')
+                    .required('Last Name is required'),
+                  email: Yup.string()
+                    .email('Invalid email address')
+                    .required('Valid email address is required'),
+                  password: Yup.string()
+                    .min(9, 'Minimum Password of 8 characters is required ')
+                    .required("This field is Required")
+                })}
+
+                onSubmit={(values, { setSubmitting }) => {
+                  setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                  }, 100);
+                }}
+              >
+                <Form className="space-y-4 md:space-y-6" action="#">
+                  {
+                    formDetails.map(({ name, type, placeholder }) =>
+                      <div key={name} className="relative mb-6 ">
+                        <label htmlFor={name} className="sr-only">Email address</label>
+                        <Field
+                          type={type}
+                          name={name}
+                          className="block min-h-[auto] w-full rounded bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear dark:text-white ring-gray-400 ring-1 focus:ring-2 focus:ring-[#A10035] focus:border-transparent"
+                          id={name}
+                          placeholder={placeholder} />
+                        <small className="dark:text-red-500">
+                          <ErrorMessage name={name} />
+                        </small>
+                      </div>
+                    )
+                  }
+                  <button type="submit" className='bg-[#A10035] hover:bg-[#fb1361] font-semibold w-full rounded p-3 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#A10035]' >
+                    SIGN UP
+                  </button>
+                </Form>
+              </Formik>
 
               <div className="flex justify-between dark:text-white">
                 <hr />OR<hr />
@@ -62,14 +81,15 @@ const SignUp = ({ formDetails }) => {
           </div>
         </div>
       </section>
+
     </>
   )
 }
 export function getStaticProps() {
 
   const formDetails = [
-    { name: "firstname", type: "text", placeholder: "Firstname" },
-    { name: "lastname", type: "text", placeholder: "Lastname" },
+    { name: "firstName", type: "text", placeholder: "Firstname" },
+    { name: "lastName", type: "text", placeholder: "Lastname" },
     { name: "email", type: "email", placeholder: "Email Address" },
     { name: "password", type: "password", placeholder: "Password" },
   ]
