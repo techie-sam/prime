137a026da550b1b1e67dc217d944161a8a562bca
TECHIE SAM <blessingabolade15@gmail.com>
2023-06-30 09:07:00 +0100

    user login and signup feature working

--- app/user/signup/page.jsx
+++ app/user/signup/page.jsx
@@ -23,7 +23,6 @@ const SignUp = () => {
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto min-h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              {/* <span className="">{error}</span> */}
               <h1 className="text-xl text-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Create Account
               </h1>
